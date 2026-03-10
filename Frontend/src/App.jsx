c35690a49464090e036747e35069afd42ca0140b
Ujjwal <ujjwa22gupta>
2026-03-10 09:03:48 +0530

    first Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import Navbar from './component/Navbar'
+import Home from './pages/Home'
+import Footer from './component/Footer'
+function App() {
+  
+
+  return (
+    <>
+    
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>
+    </>
+  )
+}
+
+export default App
